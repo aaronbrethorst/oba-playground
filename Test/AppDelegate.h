2138a9c2b9708f06269551e16b117c03513300f3
Yogi Patel <yogi.patel@gmail.com>
2015-07-07 18:01:26 -0700

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Yogi Patel on 7/7/15.
+//  Copyright (c) 2015 Yogi Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
